aa46a0a2f4609bf09837df2c910423a33eb0c67a
Yaroslav Teplov <yaroslav_teplov@mail.ru>
2024-12-13 16:22:37 +0400

    a

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import ErrorBoundary from './providers/ErrorBoundary/ErrorBoundary';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+  <HashRouter>
     <ErrorBoundary>
       <Provider store={store}>
         <App />
       </Provider>
     </ErrorBoundary>
-  </BrowserRouter>
+  </HashRouter>
 );
